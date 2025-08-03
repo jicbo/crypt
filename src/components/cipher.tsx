cb99c9bf4b68e9a35ba3361df7f7586ff87ede39
jicbo <bojicmarko06@gmail.com>
2025-08-03 15:30:21 +0200

    feat: add state management for selected cipher in CipherSolver component

--- src/components/cipher.tsx
+++ src/components/cipher.tsx
@@ -1,8 +1,10 @@
+"use client"
 import { Textarea } from "@/components/ui/textarea"
 import { Card } from "./ui/card"
 import { Combobox } from "./ui/combobox"
 import { Button } from "./ui/button"
 import { ArrowLeftRight } from "lucide-react"
+import * as React from "react"
 
 const ciphers_list = [
     { value: "ceasar", label: "Ceasar cipher" },
@@ -10,6 +12,8 @@ const ciphers_list = [
 ]
 
 export default function CipherSolver() {
+    const [selectedCipher, setSelectedCipher] = React.useState<string>("")
+
     return (
         <Card className="p-8 flex flex-col justify-center items-center">
             <div className="flex flex-row space-x-3 items-center">
@@ -19,8 +23,13 @@ export default function CipherSolver() {
                 </Button>
                 <Textarea className="resize-none"></Textarea>
             </div>
-            <Combobox placeholder_text="Ciphers" contents={ciphers_list}></Combobox>
+            <Combobox
+                placeholder_text="ciphers"
+                contents={ciphers_list}
+                onChange={setSelectedCipher} // Pass the handler
+            />
             <Button>Convert</Button>
+            {selectedCipher == "morse" ? <p>hello</p> : ""}
         </Card>
     );
 }
